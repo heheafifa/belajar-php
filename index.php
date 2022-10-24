774c1f12cc0145e2014e86deaa1644e4c2c2a826
Nur Afifa Sa'adah <nurafifaclouds@gmail.com>
2022-10-25 06:58:54 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama="Nur Afifa Sa'adah";
+$umur=22;
+$hobi="memasak dan menonton drama korea";
+
+echo "Nama saya <strong>$nama</strong>, umur saya $umur tahun, hobi saya $hobi.<br>";
+
+$namaKucing="Puma";
+$umurKucing=3;
+$hobiKucing="makan whiskas dan tidur";
+
+echo "Nama kucing saya <strong>$namaKucing</strong>, umur kucing saya $umurKucing tahun, hobi kucing saya $hobiKucing.<br>"; 
+
+$SelisihUmur=$umur-$umurKucing;
+
+echo "Selisih umur saya dan kucing saya $SelisihUmur tahun.";
+
+?>
