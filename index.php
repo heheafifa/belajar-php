9f5d8a0f4d5529ca11129481cb691cc0d671402b
Nur Afifa Sa'adah <nurafifaclouds@gmail.com>
2022-11-21 11:55:07 +0700

    add crud

--- index.php
+++ index.php
@@ -1,21 +1,55 @@
 <?php
+//1. Koneksi MySQL
+$con = mysqli_connect("localhost", "root", "", "fakultas");
 
-echo "Hello World!<br>";
+//2. Cek Koneksi
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+}else{
+    echo 'koneksi berhasil';
+}
 
-$nama="Nur Afifa Sa'adah";
-$umur=22;
-$hobi="memasak dan menonton drama korea";
+//3. Buat query
+$query = "SELECT * FROM mahasiswa";
 
-echo "Nama saya <strong>$nama</strong>, umur saya $umur tahun, hobi saya $hobi.<br>";
+// 4. Tampilkan data, dengan menjalankan sql query
+$result = mysqli_query($con,$query);
+$mahasiswa = [];
+if ($result){
+    // tampilkan data satu per satu
+    while($row = mysqli_fetch_assoc($result)){
+        $mahasiswa [] = $row;
+    }
+    mysqli_free_result($result);
+}
 
-$namaKucing="Puma";
-$umurKucing=3;
-$hobiKucing="makan whiskas dan tidur";
-
-echo "Nama kucing saya <strong>$namaKucing</strong>, umur kucing saya $umurKucing tahun, hobi kucing saya $hobiKucing.<br>"; 
-
-$SelisihUmur=$umur-$umurKucing;
-
-echo "Selisih umur saya dan kucing saya $SelisihUmur tahun.";
+//5. Tutup
+mysqli_close($con);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+</head>
+<body>
+    <h1>Data Mahasiswa</h1>
+    <table border="1" style="width:100%;">
+        <tr>
+            <th>NIM</th>
+            <th>Nama</th>
+        </tr>
+        <?php foreach($mahasiswa as $value): ?>
+        <tr>
+            <td><?php echo $value ["nim"]; ?></td>
+            <td><?php echo $value ["nama"]; ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
